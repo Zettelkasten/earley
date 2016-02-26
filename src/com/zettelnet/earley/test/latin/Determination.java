75aee1f5b15251c2ebb0aae4fbc8aa62d42cd068
Zettelkasten <Zettelkasten@localhost>
2016-02-26 17:07:02 +0100

    Added convenience constructor to Determination

--- src/com/zettelnet/earley/test/latin/Determination.java
+++ src/com/zettelnet/earley/test/latin/Determination.java
@@ -1,6 +1,7 @@
 package com.zettelnet.earley.test.latin;
 
 import com.zettelnet.latin.Form;
+import com.zettelnet.latin.FormProperty;
 import com.zettelnet.latin.lemma.Lemma;
 
 public class Determination {
@@ -8,6 +9,10 @@ public class Determination {
 	private final Lemma lemma;
 	private final Form form;
 
+	public Determination(final Lemma lemma, final FormProperty... formProperties) {
+		this(lemma, Form.withValues(formProperties));
+	}
+	
 	public Determination(final Lemma lemma, final Form form) {
 		this.lemma = lemma;
 		this.form = form;
