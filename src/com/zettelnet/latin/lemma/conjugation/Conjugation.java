bf869d9141035a8e9764c7efb4c51ba3ad51b789
Zettelkasten <Zettelkasten@localhost>
2016-03-06 22:08:23 +0100

    Added Conjugation field class

---
+++ src/com/zettelnet/latin/lemma/conjugation/Conjugation.java
@@ -0,0 +1,13 @@
+package com.zettelnet.latin.lemma.conjugation;
+
+import com.zettelnet.latin.lemma.FormProvider;
+import com.zettelnet.latin.lemma.Verb;
+
+public final class Conjugation {
+
+	public static final FormProvider<Verb> First = new FirstConjugation();
+	public static final FormProvider<Verb> Second = new SecondConjugation();
+	public static final FormProvider<Verb> Third = new ThirdConjugation();
+	public static final FormProvider<Verb> ThirdI = new ThirdIStemConjugation();
+	public static final FormProvider<Verb> Forth = new FourthConjugation();
+}
