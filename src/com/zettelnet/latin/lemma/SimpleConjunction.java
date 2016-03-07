a6c08e0604ed266e7d655332dd4f6dc41fa567e4
Zettelkasten <Zettelkasten@localhost>
2016-03-07 19:17:36 +0100

    Added simple conjunction

---
+++ src/com/zettelnet/latin/lemma/SimpleConjunction.java
@@ -0,0 +1,45 @@
+package com.zettelnet.latin.lemma;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.zettelnet.latin.form.Form;
+
+public class SimpleConjunction implements Conjunction {
+
+	private final String firstForm;
+
+	public SimpleConjunction(final String firstForm) {
+		this.firstForm = firstForm;
+	}
+
+	@Override
+	public String getFirstForm() {
+		return firstForm;
+	}
+
+	@Override
+	public Collection<String> getForm(Form form) {
+		return Arrays.asList(firstForm);
+	}
+
+	@Override
+	public boolean hasForm(Form form) {
+		return true;
+	}
+
+	@Override
+	public Map<Form, Collection<String>> getForms() {
+		Map<Form, Collection<String>> map = new HashMap<>(1);
+		map.put(Form.withValues(), Arrays.asList(firstForm));
+		return map;
+	}
+
+	@Override
+	public LemmaType getType() {
+		return LemmaType.Conjunction;
+	}
+
+}
