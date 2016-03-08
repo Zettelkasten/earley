a15db723330bbd316728a214024386c3d58c9f4b
Zettelkasten <Zettelkasten@localhost>
2016-03-09 00:16:58 +0100

    Fixed naming mistake for verb in LemmaType

--- src/com/zettelnet/latin/lemma/LemmaType.java
+++ src/com/zettelnet/latin/lemma/LemmaType.java
@@ -3,7 +3,7 @@ package com.zettelnet.latin.lemma;
 public interface LemmaType {
 
 	public static final LemmaType Noun = new SimpleLemmaType("Noun");
-	public static final LemmaType Verb = new SimpleLemmaType("Adjective");
+	public static final LemmaType Verb = new SimpleLemmaType("Verb");
 	public static final LemmaType Adverb = new SimpleLemmaType("Adverb");
 	public static final LemmaType Adjective = new SimpleLemmaType("Adjective");
 	public static final LemmaType Conjunction = new SimpleLemmaType("Conjunction");
