a307130659a0395e9bd14b1b6eb32935104a676c
Zettelkasten <Zettelkasten@localhost>
2016-03-10 23:43:11 +0100

    Added AbstractInfinitiveConjugation

---
+++ src/com/zettelnet/latin/lemma/conjugation/derivation/AbstractInfinitiveConjugation.java
@@ -0,0 +1,82 @@
+package com.zettelnet.latin.lemma.conjugation.derivation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import com.zettelnet.latin.derivation.Derivation;
+import com.zettelnet.latin.derivation.DerivationProvider;
+import com.zettelnet.latin.derivation.DerivationType;
+import com.zettelnet.latin.form.Form;
+import com.zettelnet.latin.form.Genus;
+import com.zettelnet.latin.form.Tense;
+import com.zettelnet.latin.form.Voice;
+import com.zettelnet.latin.lemma.Lemma;
+import com.zettelnet.latin.lemma.SimpleNoun;
+import com.zettelnet.latin.lemma.Verb;
+import com.zettelnet.latin.lemma.declension.Declension;
+import com.zettelnet.latin.morph.MorphProvider;
+
+public abstract class AbstractInfinitiveConjugation implements DerivationProvider<Verb> {
+
+	private final MorphProvider<Form> firstFormEndings;
+	private final MorphProvider<Form> stemEndings;
+
+	public AbstractInfinitiveConjugation(final MorphProvider<Form> firstFormEndings, final MorphProvider<Form> stemEndings) {
+		this.firstFormEndings = firstFormEndings;
+		this.stemEndings = stemEndings;
+	}
+
+	@Override
+	public Collection<Lemma> getDerivation(Verb lemma, Derivation derivation) {
+		if (derivation.getType() != DerivationType.Infinitive) {
+			return Collections.emptyList();
+		} else {
+			Collection<Lemma> lemmas = new ArrayList<>();
+
+			Form form = derivation.getForm();
+			String verbStem = lemma.getStem(Tense.Present);
+
+			for (String stemEnding : stemEndings.getMorph(form)) {
+				String firstForm = verbStem + firstFormEndings.getMorph(form).iterator().next();
+				String stem = verbStem + stemEnding;
+				
+				Lemma infinitive = new SimpleNoun(firstForm, stem, Declension.Second, Genus.Neuter);
+				lemmas.add(infinitive);
+			}
+			return lemmas;
+		}
+	}
+
+	@Override
+	public boolean hasDerivation(Verb lemma, Derivation derivation) {
+		return !getDerivation(lemma, derivation).isEmpty();
+	}
+
+	@Override
+	public Map<Derivation, Collection<Lemma>> getDerivations(Verb lemma) {
+		Map<Derivation, Collection<Lemma>> derivations = new HashMap<>();
+
+		for (Tense tense : getTenseSet(lemma)) {
+			for (Voice voice : getVoiceSet(lemma)) {
+				Derivation derivation = Derivation.withValues(DerivationType.Infinitive, tense, voice);
+				Collection<Lemma> values = getDerivation(lemma, derivation);
+				derivations.put(derivation, values);
+			}
+		}
+
+		return derivations;
+	}
+
+	public Set<Tense> getTenseSet(Verb lemma) {
+		return EnumSet.of(Tense.Present, Tense.Perfect);
+	}
+
+	public Set<Voice> getVoiceSet(Verb lemma) {
+		return EnumSet.of(Voice.Active, Voice.Passive);
+	}
+}
