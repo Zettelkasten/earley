6c83a537e76e45644d278f116ffd836d0fde5e4b
Zettelkasten <Zettelkasten@localhost>
2016-03-10 23:16:09 +0100

    Added adjectives properly

--- src/com/zettelnet/latin/lemma/SimpleAdjective.java
+++ src/com/zettelnet/latin/lemma/SimpleAdjective.java
@@ -1,11 +1,107 @@
 package com.zettelnet.latin.lemma;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.zettelnet.earley.param.property.PropertySet;
+import com.zettelnet.latin.derivation.Derivation;
+import com.zettelnet.latin.form.Casus;
+import com.zettelnet.latin.form.Comparison;
+import com.zettelnet.latin.form.Form;
 import com.zettelnet.latin.form.Genus;
+import com.zettelnet.latin.form.Numerus;
+import com.zettelnet.latin.lemma.property.LemmaProperty;
+import com.zettelnet.latin.lemma.property.MapLemmaPropertySet;
 
-public class SimpleAdjective extends AbstractDeclinableLemma implements Adjective {
+public class SimpleAdjective implements Adjective {
 
-	public SimpleAdjective(final String firstForm, final String stem, final FormProvider<DeclinableLemma> formProvider, final Genus genus) {
-		super(firstForm, stem, formProvider, genus);
+	private final Map<Genus, String> firstForm;
+	private final String stem;
+	private final Map<Genus, FormProvider<DeclinableLemma>> formProviders;
+	private final PropertySet<LemmaProperty> properties;
+
+	private static <T> Map<Genus, T> makeGenusMap(T masculine, T feminine, T neuter) {
+		Map<Genus, T> map = new EnumMap<>(Genus.class);
+		map.put(Genus.Masculine, masculine);
+		map.put(Genus.Feminine, feminine);
+		map.put(Genus.Neuter, neuter);
+		return map;
+	}
+
+	public SimpleAdjective(final String masculineForm, final String feminineForm, final String neuterForm, final String stem, final Map<Genus, FormProvider<DeclinableLemma>> formProvider, LemmaProperty... lemmaProperties) {
+		this(makeGenusMap(masculineForm, feminineForm, neuterForm), stem, formProvider);
+	}
+
+	public SimpleAdjective(final String masculineForm, final String feminineForm, final String neuterForm, final String stem, final FormProvider<DeclinableLemma> formProvider, LemmaProperty... lemmaProperties) {
+		this(makeGenusMap(masculineForm, feminineForm, neuterForm), stem, makeGenusMap(formProvider, formProvider, formProvider));
+	}
+
+	public SimpleAdjective(final Map<Genus, String> firstForm, final String stem, final Map<Genus, FormProvider<DeclinableLemma>> formProvider, LemmaProperty... lemmaProperties) {
+		this.firstForm = firstForm;
+		this.stem = stem;
+		this.formProviders = formProvider;
+		this.properties = MapLemmaPropertySet.valueOf(lemmaProperties);
+	}
+
+	@Override
+	public String getFirstForm() {
+		return getForm(Form.withValues(Casus.Nominative, Numerus.Singular, Genus.Masculine, Comparison.Positive)).iterator().next();
+	}
+
+	public String getFirstForm(Genus genus) {
+		return firstForm.get(genus);
+	}
+
+	@Override
+	public Collection<String> getForm(Form form) {
+		return formProviders.get(form.getGenus()).getForm(this, form);
+	}
+
+	@Override
+	public boolean hasForm(Form form) {
+		return formProviders.get(form.getGenus()).hasForm(this, form);
+	}
+
+	@Override
+	public Map<Form, Collection<String>> getForms() {
+		Map<Form, Collection<String>> forms = new HashMap<>();
+		for (Map.Entry<Genus, FormProvider<DeclinableLemma>> entry : formProviders.entrySet()) {
+			forms.putAll(entry.getValue().getForms(this));
+		}
+		return forms;
+	}
+
+	@Override
+	public Collection<Lemma> getDerivation(Derivation derivation) {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public boolean hasDerivation(Derivation derivation) {
+		return false;
+	}
+
+	@Override
+	public Map<Derivation, Collection<Lemma>> getDerivations() {
+		return Collections.emptyMap();
+	}
+
+	@Override
+	public PropertySet<LemmaProperty> getProperties() {
+		return properties;
+	}
+
+	@Override
+	public String getStem() {
+		return stem;
+	}
+
+	@Override
+	public Genus getGenus() {
+		return null;
 	}
 
 	@Override
@@ -17,4 +113,5 @@ public class SimpleAdjective extends AbstractDeclinableLemma implements Adjectiv
 	public String toString() {
 		return getFirstForm();
 	}
+
 }
--- src/com/zettelnet/latin/lemma/declension/Declension.java
+++ src/com/zettelnet/latin/lemma/declension/Declension.java
@@ -1,5 +1,9 @@
 package com.zettelnet.latin.lemma.declension;
 
+import java.util.EnumMap;
+import java.util.Map;
+
+import com.zettelnet.latin.form.Genus;
 import com.zettelnet.latin.lemma.DeclinableLemma;
 import com.zettelnet.latin.lemma.FormProvider;
 
@@ -12,4 +16,11 @@ public final class Declension {
 	public static final FormProvider<DeclinableLemma> ThirdMixed = new ThirdMixedDeclension();
 	public static final FormProvider<DeclinableLemma> Forth = new FourthDeclension();
 	public static final FormProvider<DeclinableLemma> Fifth = new FifthDeclension();
+	
+	public static final Map<Genus, FormProvider<DeclinableLemma>> FirstAndSecond = new EnumMap<>(Genus.class);
+	static {
+		FirstAndSecond.put(Genus.Masculine, Second);
+		FirstAndSecond.put(Genus.Feminine, First);
+		FirstAndSecond.put(Genus.Neuter, Second);
+	}
 }
