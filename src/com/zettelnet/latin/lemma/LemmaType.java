a354a02c8fd1ce8357e7ee712eb5f76515d077fd
Zettelkasten <Zettelkasten@localhost>
2016-03-12 21:13:04 +0100

    Added verbal noun lemma types

--- src/com/zettelnet/latin/lemma/LemmaType.java
+++ src/com/zettelnet/latin/lemma/LemmaType.java
@@ -4,6 +4,9 @@ public interface LemmaType {
 
 	public static final LemmaType Noun = new SimpleLemmaType("Noun");
 	public static final LemmaType Verb = new SimpleLemmaType("Verb");
+	public static final LemmaType Participle = new SimpleLemmaType("Participle");
+	public static final LemmaType Infinitive = new SimpleLemmaType("Infinitive");
+	public static final LemmaType Supine = new SimpleLemmaType("Supine");
 	public static final LemmaType Adverb = new SimpleLemmaType("Adverb");
 	public static final LemmaType Adjective = new SimpleLemmaType("Adjective");
 	public static final LemmaType Conjunction = new SimpleLemmaType("Conjunction");
