094cc861c841ee23f7bc1bc15ec461e60bd6f945
Zettelkasten <Zettelkasten@localhost>
2016-03-12 21:39:01 +0100

    Added infinitive conjugation concrete implementations

---
+++ src/com/zettelnet/latin/lemma/conjugation/derivation/FourthInfinitiveConjugation.java
@@ -0,0 +1,38 @@
+package com.zettelnet.latin.lemma.conjugation.derivation;
+
+import java.util.Arrays;
+
+import com.zettelnet.latin.form.FormValueProvider;
+import com.zettelnet.latin.form.MapFormValueProvider;
+import com.zettelnet.latin.form.Tense;
+import com.zettelnet.latin.form.Voice;
+import com.zettelnet.latin.lemma.VerbStem;
+
+public class FourthInfinitiveConjugation extends AbstractInfinitiveConjugation {
+
+	private static class FirstFormEndings extends MapFormValueProvider<String> {
+		public FirstFormEndings() {
+			super(Arrays.asList(Tense.class, Voice.class));
+
+			put("_ire", Tense.Present, Voice.Active);
+			put("_ir_i", Tense.Present, Voice.Passive);
+			put("_ivisse", Tense.Perfect, Voice.Active);
+		}
+	}
+
+	private static class StemEndings extends MapFormValueProvider<String> {
+		public StemEndings() {
+			super(Arrays.asList(Tense.class, Voice.class));
+
+			put("iend", Tense.Present, Voice.Active);
+		}
+	}
+
+	public static final FormValueProvider<String> FIRST_FORM_ENDINGS = new FirstFormEndings();
+	public static final FormValueProvider<String> STEM_ENDINGS = new StemEndings();
+	public static final FormValueProvider<VerbStem> STEM_TYPES = new InfinitiveStems();
+
+	public FourthInfinitiveConjugation() {
+		super(FIRST_FORM_ENDINGS, STEM_ENDINGS, STEM_TYPES);
+	}
+}
---
+++ src/com/zettelnet/latin/lemma/conjugation/derivation/InfinitiveStems.java
@@ -0,0 +1,19 @@
+package com.zettelnet.latin.lemma.conjugation.derivation;
+
+import java.util.Arrays;
+
+import com.zettelnet.latin.form.MapFormValueProvider;
+import com.zettelnet.latin.form.Tense;
+import com.zettelnet.latin.form.Voice;
+import com.zettelnet.latin.lemma.VerbStem;
+
+public class InfinitiveStems extends MapFormValueProvider<VerbStem> {
+	
+	public InfinitiveStems() {
+		super(Arrays.asList(Tense.class, Voice.class));
+
+		put(VerbStem.Present, Tense.Present, Voice.Active);
+		put(VerbStem.Present, Tense.Present, Voice.Passive);
+		put(VerbStem.Perfect, Tense.Perfect, Voice.Active);
+	}
+}
