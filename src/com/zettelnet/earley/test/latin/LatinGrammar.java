5440fee50bbdcc798e47d638728fbb3054dcc776
Zettelkasten <Zettelkasten@localhost>
2016-03-22 23:48:13 +0100

    Added LatinHelper to simplify latin grammar code

--- src/com/zettelnet/earley/test/latin/LatinGrammar.java
+++ src/com/zettelnet/earley/test/latin/LatinGrammar.java
@@ -1,5 +1,9 @@
 package com.zettelnet.earley.test.latin;
 
+import static com.zettelnet.earley.test.latin.LatinHelper.copy;
+import static com.zettelnet.earley.test.latin.LatinHelper.key;
+import static com.zettelnet.earley.test.latin.LatinHelper.specify;
+
 import com.zettelnet.earley.Grammar;
 import com.zettelnet.earley.ParameterizedSymbol;
 import com.zettelnet.earley.SimpleGrammar;
@@ -7,13 +11,10 @@ import com.zettelnet.earley.param.AnyParameterExpression;
 import com.zettelnet.earley.param.CopyParameterExpression;
 import com.zettelnet.earley.param.ParameterExpression;
 import com.zettelnet.earley.param.ParameterManager;
-import com.zettelnet.earley.param.SingletonParameterFactory;
-import com.zettelnet.earley.param.SpecificParameterExpression;
 import com.zettelnet.earley.param.TokenParameterizer;
 import com.zettelnet.earley.symbol.NonTerminal;
 import com.zettelnet.earley.symbol.SimpleNonTerminal;
 import com.zettelnet.earley.symbol.Terminal;
-import com.zettelnet.earley.test.latin.individual.IndividualFormParameterExpression;
 import com.zettelnet.latin.form.Casus;
 import com.zettelnet.latin.form.Numerus;
 import com.zettelnet.latin.form.Tense;
@@ -52,7 +53,7 @@ public final class LatinGrammar {
 		TokenParameterizer<Token, FormParameter> parameterizer = new FormParameterizer();
 
 		SimpleGrammar<Token, FormParameter> grammar = new SimpleGrammar<>(sentence, parameterManager);
-		grammar.setStartSymbolParameter(new SingletonParameterFactory<>(new FormParameter(Casus.Nominative, Finiteness.Finite)));
+		grammar.setStartSymbolParameter(key(Casus.Nominative, Finiteness.Finite));
 
 		ParameterExpression<Token, FormParameter> copy = new CopyParameterExpression<>(grammar, parameterizer);
 		ParameterExpression<Token, FormParameter> any = new AnyParameterExpression<>(parameterManager);
@@ -77,48 +78,48 @@ public final class LatinGrammar {
 		// VF(pi) -> v(pi)
 		grammar.addProduction(
 				verbForm,
-				new SingletonParameterFactory<>(new FormParameter(Finiteness.Finite)),
+				key(Finiteness.Finite),
 				new ParameterizedSymbol<>(verb, copy));
 		// TODO
 		grammar.addProduction(
 				verbForm,
-				new SingletonParameterFactory<>(new FormParameter(Finiteness.Infinitive)),
-				new ParameterizedSymbol<>(infinitive, new SpecificParameterExpression<>(parameterManager, parameterizer, new FormParameter(Casus.Nominative))));
+				key(Finiteness.Infinitive),
+				new ParameterizedSymbol<>(infinitive, specify(parameterManager, parameterizer, Casus.Nominative)));
 
 		// Args(pi : NullVal) -> epsilon
 		grammar.addProduction(
 				arguments,
-				new SingletonParameterFactory<>(new FormParameter(Valency.Null)));
+				key(Valency.Null));
 		// Args(pi : SingleVal) -> epsilon
 		grammar.addProduction(
 				arguments,
-				new SingletonParameterFactory<>(new FormParameter(Valency.Single)));
+				key(Valency.Single));
 		// Args(pi : Kopula) -> NP(pi)
 		grammar.addProduction(
 				arguments,
-				new SingletonParameterFactory<>(new FormParameter(Valency.Copula)),
-				new ParameterizedSymbol<>(nounPhrase, new SpecificParameterExpression<>(parameterManager, parameterizer, new FormParameter(Casus.Nominative))));
+				key(Valency.Copula),
+				new ParameterizedSymbol<>(nounPhrase, specify(parameterManager, parameterizer, Casus.Nominative)));
 		// Args(pi : GenVal) -> NP(Gen)
 		grammar.addProduction(
 				arguments,
-				new SingletonParameterFactory<>(new FormParameter(Valency.Genitive)),
-				new ParameterizedSymbol<>(nounPhrase, new SpecificParameterExpression<>(parameterManager, parameterizer, new FormParameter(Casus.Genitive))));
+				key(Valency.Genitive),
+				new ParameterizedSymbol<>(nounPhrase, specify(parameterManager, parameterizer, Casus.Genitive)));
 		// Args(pi : DatVal) -> NP(Dat)
 		grammar.addProduction(
 				arguments,
-				new SingletonParameterFactory<>(new FormParameter(Valency.Dative)),
-				new ParameterizedSymbol<>(nounPhrase, new SpecificParameterExpression<>(parameterManager, parameterizer, new FormParameter(Casus.Dative))));
+				key(Valency.Dative),
+				new ParameterizedSymbol<>(nounPhrase, specify(parameterManager, parameterizer, Casus.Dative)));
 		// Args(pi : AkkVal) -> NP(Akk)
 		grammar.addProduction(
 				arguments,
-				new SingletonParameterFactory<>(new FormParameter(Valency.Accusative)),
-				new ParameterizedSymbol<>(nounPhrase, new SpecificParameterExpression<>(parameterManager, parameterizer, new FormParameter(Casus.Accusative))));
+				key(Valency.Accusative),
+				new ParameterizedSymbol<>(nounPhrase, specify(parameterManager, parameterizer, Casus.Accusative)));
 		// Args(pi : AkkDatVal) -> NP(Akk) NP(Dat)
 		grammar.addProduction(
 				arguments,
-				new SingletonParameterFactory<>(new FormParameter(Valency.AccusativeDative)),
-				new ParameterizedSymbol<>(nounPhrase, new SpecificParameterExpression<>(parameterManager, parameterizer, new FormParameter(Casus.Accusative))),
-				new ParameterizedSymbol<>(nounPhrase, new SpecificParameterExpression<>(parameterManager, parameterizer, new FormParameter(Casus.Dative))));
+				key(Valency.AccusativeDative),
+				new ParameterizedSymbol<>(nounPhrase, specify(parameterManager, parameterizer, Casus.Accusative)),
+				new ParameterizedSymbol<>(nounPhrase, specify(parameterManager, parameterizer, Casus.Dative)));
 
 		// AP var
 		grammar.addProduction(
@@ -135,7 +136,7 @@ public final class LatinGrammar {
 		// NP(pi : Fin) -> NF(pi) Attr(pi)*
 		grammar.addProduction(
 				nounPhrase,
-				new SingletonParameterFactory<>(new FormParameter(Finiteness.Finite)),
+				key(Finiteness.Finite),
 				new ParameterizedSymbol<>(nounForm, copy),
 				new ParameterizedSymbol<>(attributeVar, copy));
 		// NF(pi) -> n(pi)
@@ -160,29 +161,29 @@ public final class LatinGrammar {
 		// Attr(pi) -> NP(Gen)
 		grammar.addProduction(
 				attribute,
-				new ParameterizedSymbol<>(nounPhrase, new SpecificParameterExpression<>(parameterManager, parameterizer, new FormParameter(Casus.Genitive))));
+				new ParameterizedSymbol<>(nounPhrase, specify(parameterManager, parameterizer, Casus.Genitive)));
 		// NP(pi : Nom / Akk) -> S(pi : Inf Pr�s/Perf/Fut Akk)
 		grammar.addProduction(
 				nounPhrase,
-				new SingletonParameterFactory<>(new FormParameter(Casus.Nominative, Casus.Accusative)),
-				new ParameterizedSymbol<>(sentence, new SpecificParameterExpression<>(parameterManager, parameterizer, new FormParameter(Casus.Accusative, Tense.Present, Tense.Perfect, Tense.Future, Finiteness.Infinitive))));
+				key(Casus.Nominative, Casus.Accusative),
+				new ParameterizedSymbol<>(sentence, specify(parameterManager, parameterizer, Casus.Accusative, Tense.Present, Tense.Perfect, Tense.Future, Finiteness.Infinitive)));
 
 		// coordinations
 
 		// NP(pi : Pl) -> NP(?) conj NP(?)
 		grammar.addProduction(
 				nounPhrase,
-				new SingletonParameterFactory<>(new FormParameter(Numerus.Plural)),
-				new ParameterizedSymbol<>(nounPhrase, new IndividualFormParameterExpression<>(parameterizer).copy(Casus.class)),
+				key(Numerus.Plural),
+				new ParameterizedSymbol<>(nounPhrase, copy(parameterizer, Casus.class)),
 				new ParameterizedSymbol<>(conjunction, any),
-				new ParameterizedSymbol<>(nounPhrase, new IndividualFormParameterExpression<>(parameterizer).copy(Casus.class)));
+				new ParameterizedSymbol<>(nounPhrase, copy(parameterizer, Casus.class)));
 
 		// VP(pi) -> VP(pi) conj VP(pi)
 		grammar.addProduction(
 				verbPhrase,
-				new ParameterizedSymbol<>(verbPhrase, new IndividualFormParameterExpression<>(parameterizer).copy(Numerus.class).copy(Finiteness.class)),
+				new ParameterizedSymbol<>(verbPhrase, copy(parameterizer, Numerus.class, Finiteness.class)),
 				new ParameterizedSymbol<>(conjunction, any),
-				new ParameterizedSymbol<>(verbPhrase, new IndividualFormParameterExpression<>(parameterizer).copy(Numerus.class).copy(Finiteness.class)));
+				new ParameterizedSymbol<>(verbPhrase, copy(parameterizer, Numerus.class, Finiteness.class)));
 
 		return grammar;
 	}
