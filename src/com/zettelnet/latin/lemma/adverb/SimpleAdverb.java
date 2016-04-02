8869cb3dd347e095625bc77ae2d186d3d1fe4a9c
Zettelkasten <Zettelkasten@localhost>
2016-04-02 22:20:15 +0200

    Fixed SimpleAdverb not implementing Adverb

--- src/com/zettelnet/latin/lemma/adverb/SimpleAdverb.java
+++ src/com/zettelnet/latin/lemma/adverb/SimpleAdverb.java
@@ -14,7 +14,7 @@ import com.zettelnet.latin.lemma.Lemma;
 import com.zettelnet.latin.lemma.LemmaType;
 import com.zettelnet.latin.lemma.property.LemmaProperty;
 
-public class SimpleAdverb implements Lemma {
+public class SimpleAdverb implements Adverb {
 
 	private final String firstForm;
 	private final PropertySet<LemmaProperty> properties;
