11d139d93067a75a4ba9c51f7a10693d716be488
Zettelkasten <Zettelkasten@localhost>
2016-04-10 12:57:54 +0200

    Added SimpleInterjection

---
+++ src/com/zettelnet/latin/lemma/interjection/SimpleInterjection.java
@@ -0,0 +1,93 @@
+package com.zettelnet.latin.lemma.interjection;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.zettelnet.earley.param.property.MapPropertySet;
+import com.zettelnet.earley.param.property.PropertySet;
+import com.zettelnet.latin.derivation.Derivation;
+import com.zettelnet.latin.form.Form;
+import com.zettelnet.latin.lemma.Lemma;
+import com.zettelnet.latin.lemma.LemmaType;
+import com.zettelnet.latin.lemma.property.LemmaProperty;
+
+public class SimpleInterjection implements Interjection {
+
+	private final String firstForm;
+	private final PropertySet<LemmaProperty> properties;
+
+	public SimpleInterjection(final String firstForm, final LemmaProperty... properties) {
+		this.firstForm = firstForm;
+		this.properties = MapPropertySet.withValues(properties);
+	}
+
+	@Override
+	public String getNominalForm() {
+		return firstForm;
+	}
+
+	@Override
+	public Collection<String> getForm(Form form) {
+		return Arrays.asList(firstForm);
+	}
+
+	@Override
+	public boolean hasForm(Form form) {
+		return true;
+	}
+
+	@Override
+	public Map<Form, Collection<String>> getForms() {
+		Map<Form, Collection<String>> map = new HashMap<>(1);
+		map.put(Form.withValues(), Arrays.asList(firstForm));
+		return map;
+	}
+
+	@Override
+	public Collection<Lemma> getDerivation(Derivation derivation) {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public boolean hasDerivation(Derivation derivation) {
+		return false;
+	}
+
+	@Override
+	public Map<Derivation, Collection<Lemma>> getDerivations() {
+		return Collections.emptyMap();
+	}
+
+	@Override
+	public PropertySet<LemmaProperty> getProperties() {
+		return properties;
+	}
+
+	@Override
+	public LemmaType getType() {
+		return LemmaType.Interjection;
+	}
+
+	@Override
+	public boolean isDerivation() {
+		return false;
+	}
+
+	@Override
+	public Lemma getDerivedFrom() {
+		return null;
+	}
+	
+	@Override
+	public Derivation getDerivationKind() {
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		return getNominalForm();
+	}
+}
