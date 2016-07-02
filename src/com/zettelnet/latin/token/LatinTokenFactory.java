da9932bf85708ae593861d2afae10ccdd93335c3
Zettelkasten <Zettelkasten@localhost>
2016-07-02 18:58:05 +0200

    Added LatinTokenFactory

---
+++ src/com/zettelnet/latin/token/LatinTokenFactory.java
@@ -0,0 +1,28 @@
+package com.zettelnet.latin.token;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+
+import com.zettelnet.earley.token.TokenFactory;
+import com.zettelnet.latin.form.Form;
+import com.zettelnet.latin.lemma.property.Meaning;
+import com.zettelnet.latin.param.FormParameter;
+
+public class LatinTokenFactory implements TokenFactory<Token, FormParameter> {
+
+	@Override
+	public Collection<Token> makeToken(FormParameter parameter) {
+		Collection<Token> tokens = new ArrayList<>();
+		Form form = parameter.toForm();
+		// TODO untested
+		Set<Meaning> meaningSet = parameter.getProperty(Meaning.TYPE);
+		for (Meaning meaning : meaningSet) {
+			for (String value : meaning.getLemma().getForm(form)) {
+				Determination determination = new Determination(meaning.getLemma(), form);
+				tokens.add(new Token(value, determination));
+			}
+		}
+		return tokens;
+	}
+}
