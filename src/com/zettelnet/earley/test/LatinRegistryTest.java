dbab7903c8f72f510fb90c043d0bed0edf8a68d9
Zettelkasten <Zettelkasten@localhost>
2016-07-22 15:04:48 +0200

    LatinRegistryTests now uses meaning

--- src/com/zettelnet/earley/test/LatinRegistryTest.java
+++ src/com/zettelnet/earley/test/LatinRegistryTest.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.zettelnet.earley.ChartSetPrinter;
 import com.zettelnet.earley.EarleyParser;
@@ -26,6 +28,7 @@ import com.zettelnet.earley.tree.SyntaxTrees;
 import com.zettelnet.german.token.GermanDetermination;
 import com.zettelnet.german.token.GermanToken;
 import com.zettelnet.latin.grammar.LatinGrammar;
+import com.zettelnet.latin.lemma.property.Meaning;
 import com.zettelnet.latin.param.FormParameter;
 import com.zettelnet.latin.token.LatinTokenPrinter;
 import com.zettelnet.latin.token.Token;
@@ -59,7 +62,18 @@ public class LatinRegistryTest {
 		TokenFactory<GermanToken, FormParameter> germanTokenFactory = new TokenFactory<GermanToken, FormParameter>() {
 			@Override
 			public Collection<GermanToken> makeToken(Terminal<GermanToken> terminal, FormParameter parameter) {
-				return Arrays.asList(new GermanToken("DE_{" + terminal + ":" + parameter + "}", new GermanDetermination(null, parameter.toForm())));
+				Set<Meaning> meanings = parameter.getProperty(Meaning.TYPE);
+
+				if (meanings.isEmpty()) {
+					return Arrays.asList(new GermanToken("DE_{" + terminal + ":" + parameter + "}", new GermanDetermination(null, parameter.toForm())));
+				} else {
+					Collection<GermanToken> tokens = new HashSet<>();
+					for (Meaning meaning : meanings) {
+						String content = LatinRegistry.getTranslation(meaning.getLemma());
+						tokens.add(new GermanToken(content, new GermanDetermination(null, parameter.toForm())));
+					}
+					return tokens;
+				}
 			}
 		};
 		Translator<Token, FormParameter, GermanToken, FormParameter> germanTranslator = new SimpleTranslator<>(grammar, germanTokenFactory, germanTranslations);
